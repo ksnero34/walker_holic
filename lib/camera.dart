b8b66df92346ab8439a9667c95262425d7291ed4
mins <ksnero34@naver.com>
2021-05-20 18:36:34 +0900

    d

--- lib/camera.dart
+++ lib/camera.dart
@@ -3,8 +3,8 @@ import 'package:flutter/cupertino.dart';
 class camera extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Front Page'),
-    );
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('Camera Page'), Text('이선우 대머리')]);
   }
 }
