ea385615205341c8222186f191e253fb22122193
mins <ksnero34@naver.com>
2021-05-23 17:21:40 +0900

    ios 해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,31 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:walkerholic/badge.dart';
 import 'package:walkerholic/camera.dart';
 //import 'package:walkerholic/location/user_location.dart';
 import 'package:background_location/background_location.dart';
+import 'package:walkerholic/location/getnearest.dart';
 
 import 'home.dart';
 import 'location/locationservice.dart';
 import 'location/user_location.dart';
 
 LatLng userlocation_global;
+getnearestsite nearestlocation_global;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  BackgroundLocation.setAndroidNotification(
-	title: "Notification title",
-        message: "Notification message",
-        icon: "@mipmap/ic_launcher",
-);
-BackgroundLocation.setAndroidConfiguration(1000);
-BackgroundLocation.startLocationService();
-BackgroundLocation.getLocationUpdates((location) {
-  print(location);
-});
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<UserLocation>(
-      create: (context) => LocationService().locationStream,
-      //      builder: (context) => LocationService().locationStream,
-      child: CupertinoApp(
-        // Remove the debug
-        debugShowCheckedModeBanner: false,
-        title: '부산폴짝',
-        home: MyHomePage(),
-      ),
+    return CupertinoApp(
+      // Remove the debug
+      debugShowCheckedModeBanner: false,
+      title: '부산폴짝',
+      home: MyHomePage(),
     );
   }
 }
@@ -56,12 +46,29 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    BackgroundLocation.startLocationService();
+
+    BackgroundLocation.getLocationUpdates((location) async {
+      userlocation_global = LatLng(location.latitude, location.longitude);
+      nearestlocation_global.setnearest(userlocation_global);
+      print('root단 호출, 위치');
+      print(nearestlocation.site);
+      //compute(nearestlocation_global.setnearest, userlocation_global);
+      //print(userlocation_global.longitude);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final double statusBarHeight =
         MediaQuery.of(context).padding.top; //기기의 상태창 크기
     final double statusHeight = (MediaQuery.of(context).size.height -
         statusBarHeight -
         MediaQuery.of(context).padding.bottom); // 기기의 화면크기
+
     return CupertinoPageScaffold(
       child: CupertinoTabScaffold(
           tabBar: CupertinoTabBar(
@@ -82,3 +89,23 @@ class _MyHomePageState extends State<MyHomePage> {
 //tabBuilder: (BuildContext context, index) {
 //          return _pages[index];
 //      }
+
+/*
+위치 스트림
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<UserLocation>(
+      create: (context) => LocationService().locationStream,
+      //      builder: (context) => LocationService().locationStream,
+      child: CupertinoApp(
+        // Remove the debug
+        debugShowCheckedModeBanner: false,
+        title: '부산폴짝',
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+*/
